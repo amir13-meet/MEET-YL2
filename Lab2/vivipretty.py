a3db2ab5d337e1f50250aace0815d4a4ab0b67bb
Guest <guest-05Pr0y@meet.mit.edu>
2014-11-11 19:52:55 +0200

    Done with Sesh 2

--- Lab2/vivipretty.py
+++ Lab2/vivipretty.py
@@ -1,6 +1,35 @@
-class Girl(object):
-	def __init__(self,name,age,pretty):
+class Animal():
+	def __init__(self,name,age):
 		self.name = name
 		self.age = age
-		self.pretty  = pretty
 
+	def sleep(self):
+		print self.name+" is "+str(self.age)+" years old, and is sleeping now."
+
+	def eat(self,food):
+		print self.name+" is now eating "+food
+
+#-----------------------------------------------
+
+class Bird(Animal):
+	Animal.__init__(self,name,age,w_color):
+		self.w_color = w_color
+
+	def fly(self):
+		self.name+" with "+self.w_color+" color wings is flying"
+
+#-----------------------------------------------
+
+class Dog(Animal):
+	Animal.__init__(self,name,age):
+
+	def bark(self):
+		self.name+" is barking"
+
+#-----------------------------------------------
+
+l = Dog("Lucky",4)
+l.bark()
+
+c = Bird("Stacy",1,"Blue")
+c.fly()
